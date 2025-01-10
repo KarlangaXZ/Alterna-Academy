8e9c721f7dc9b9862c7b733a70d172882e73963b
KarlangaXZ <carlos.josewin@gmail.com>
2025-01-10 18:08:20 -0400

    bad request!

--- modulo 8/SimpleAPI-Day1/todo/obj/Debug/net9.0/todo.AssemblyInfo.cs
+++ modulo 8/SimpleAPI-Day1/todo/obj/Debug/net9.0/todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+810a7678e3e5d4cb8660e8b92c2f7593002611d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed3633437a0d1c01a0dcad8c111e05073a9534e")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
