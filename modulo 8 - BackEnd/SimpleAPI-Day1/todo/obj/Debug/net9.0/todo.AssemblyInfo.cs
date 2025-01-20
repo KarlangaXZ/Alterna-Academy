742e58e0e5ea1d8be0fd9458a07ccd24667f7959
KarlangaXZ <carlos.josewin@gmail.com>
2025-01-20 18:47:37 -0400

    name changes

--- modulo 8 - BackEnd/SimpleAPI-Day1/todo/obj/Debug/net9.0/todo.AssemblyInfo.cs
+++ modulo 8 - BackEnd/SimpleAPI-Day1/todo/obj/Debug/net9.0/todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d6ee62911d079a90e06045c31a3fa23a7a100a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a99683b9b1c7377678af63ab040bfbb4613c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
